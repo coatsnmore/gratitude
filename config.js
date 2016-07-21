f832c0505bfb8cf9ec673776d3bc502aba723afd
coats.18@gmail.com <COATSN@NW108914.nwie.net>
2016-07-21 15:57:50 -0400

    intiial commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "https://test-nw-hackathon.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "8yYgb0TEdfdwF0px12EUDnqoZw2KrDtRNCwzr7MB9Ja9yU5dswobsxalD0ZIGgfz5KcSSLU8xP55xDpG5dmWEw==";
+config.host = process.env.HOST || "https://gratitude-db.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "skFgpBLkGySoaes7m9ThvnirANCu9vB3ySAAeSCn4utRI2odiyvVMvK6ou4winYOvhFpDzawyK0mjYUWtveWAg==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
