3760cee407196a1e645f282f49bdc5fec9075d42
coats.18@gmail.com <COATSN@NW108914.nwie.net>
2016-07-22 05:59:40 -0400

    added animated gif

--- public/src/app/welcomeController.js
+++ public/src/app/welcomeController.js
@@ -1,13 +1,22 @@
 /**
  * Simple note music player
  */
-angular.module('gratitude').controller('WelcomeController', ['$scope', '$location',
-	function($scope, $location) {
-		$scope.select = function (){
-      $location.path('/select');
-    };
-    $scope.login = function (){
-      $location.path('/login');
-    };
+angular.module('gratitude').controller('WelcomeController', ['$scope', '$location', '$timeout',
+	function($scope, $location, $timeout) {
+		// $scope.select = function (){
+    //   $location.path('/select');
+    // };
+    // $scope.login = function (){
+    //   $location.path('/login');
+    // };
+		// window.setTimeout(function (){
+		// 	console.log('logging in...');
+		// 	$location.path('#/login');
+		// }, 7000);
+
+		$timeout(function(){
+		  // logged_in = true;
+		  $location.path("/login");
+		},7000);
 	}
 ]);
