0a31768a3515e46fbe36beece0c908dba910fce2
coats.18@gmail.com <COATSN@NW108914.nwie.net>
2016-07-21 23:34:21 -0400

    added login page

--- public/src/app/loginController.js
+++ public/src/app/loginController.js
@@ -3,8 +3,15 @@
  */
 angular.module('gratitude').controller('LoginController', ['$scope', '$location',
 	function($scope, $location) {
-		$scope.select = function (){
-      $location.path('/select');
-    };
+		// $scope.select = function (){
+    //   $location.path('/select');
+    // };
+		$scope.userName = null;
+		$scope.password = null;
+
+		$scope.login = function (){
+			console.log('un: ' + userName);
+			console.log('pw: ' + password);
+		};
 	}
 ]);
